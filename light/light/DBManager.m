b38133ac5f420ad57cb8518f7db85e06f39b6410
ericgiannini <eric.giannini@yahoo.com>
2017-05-07 13:43:31 -0700

    Programmed initWithDatabaseFilename

--- light/light/DBManager.m
+++ light/light/DBManager.m
@@ -13,7 +13,15 @@
 @interface DBManager ()
 
 //extension of DBManager() with private class files
+
+/* 
+ 
+ There is no sense in accessing the database directly; these 
+ properties facilate access to a copy.
+ 
+*/
 @property (nonatomic, strong) NSString *documentDirectory;
+
 @property (nonatomic, strong) NSString *databaseFilename;
 
 @end
@@ -23,6 +31,18 @@
 - (instancetype)initWithDatabaseFilename:(NSString *)dbFilename {
     self = [super init];
     if (self) {
+        
+        // Set the documents directoy path to the documentsDirectory property.
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        self.documentDirectory = [paths objectAtIndex:0];
+        
+        // assign global variable for filepath to its local counterpart
+        self.databaseFilename = dbFilename;
+        
+        // Copy the database file into the documents directory if necessary
+        [self copyDatabaseIntoDocumentsDirectory];
+        
+        
         
     }
     
